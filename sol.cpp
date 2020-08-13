bc05c0cb2d0eebf176affe7b584be25e163179c4
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-08-13 13:06:49 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -9,7 +9,6 @@ int main() {
 	cin >> a >> b >> c;
 	float x1 = ((-1 * b) + sqrt(((b * b) - 4 * a * c))) / (2 * a);
 	float x2 = ((-1 * b) - sqrt(((b * b) - 4 * a * c))) / (2 * a);
-	cout << fixed << setprecision(6) << x1 << '\n';
-	cout << fixed << setprecision(6) << x2 << '\n';
+	cout << fixed << setprecision(6) << x1 << '\n' << x2 << '\n';
 	return 0;
 }
